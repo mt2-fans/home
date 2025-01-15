4a36b2041d4e667df0674a885940a81a5fb47763
mt2-fans <tomaskenji2010@gmail.com>
2025-01-14 21:01:24 -0300

    pronto

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
-    $servername = '10.0.0.100';
-    $username = 'User';
-    $password = '123';
+    $servername = '200.9.154.115';
+    $username = 'root';
+    $password = 'Camelia@4213';
     $dbaccount = 'account'; //Não alterar
     $dbplayer = 'player';   //Não alterar
     $dbcommon = 'common';   //Não alterar
