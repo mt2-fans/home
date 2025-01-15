a4f6e8b4c6c84d6fcda09c76ac22a0d9ccea8775
mt2-fans <tomaskenji2010@gmail.com>
2025-01-14 22:21:02 -0300

    Delete site directory

--- site/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = '10.0.0.100';
-    $username = 'User';
-    $password = '123';
-    $dbaccount = 'account'; //Não alterar
-    $dbplayer = 'player';   //Não alterar
-    $dbcommon = 'common';   //Não alterar
-?>
